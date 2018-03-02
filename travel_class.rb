55fb32f15b362a8230617f5f7c797da6730dd211
Clayton-ty <c.waldock@outlook.com>
2018-03-02 10:56:51 +1000

    added comments

--- travel_class.rb
+++ travel_class.rb
@@ -19,14 +19,16 @@ class Travel
   attr_accessor :travel_cost, :distance
 
   def initialize (end_location)
+    begin
     start_location = Geocoder.coordinates("brisbane")
     end_location_coordinates = Geocoder.coordinates(end_location)
     # works out distance traveled from start location to end location
     @distance = Geocoder::Calculations.distance_between(start_location, end_location_coordinates)
     # works out cost per km
     @travel_cost = (distance * 1.2).round(2)
+  rescue
+    puts "Sorry we seem to have lost connection, please try again later."
+  end
 
-  end  
+  end
 end
-
-
